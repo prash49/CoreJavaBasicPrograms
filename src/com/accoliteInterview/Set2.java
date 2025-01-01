e91e16a4dda2ecc0b58d28e6f00de9b6b363bd64
Prashanth N <imprash4u@gmail.com>
2025-01-01 20:32:34 +0530

    accolite interview

---
+++ src/com/accoliteInterview/Set2.java
@@ -0,0 +1,15 @@
+package com.accoliteInterview;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Set2 {
+
+    public static void main(String[] args) {
+        // insert code here
+        Set set =  new HashSet<>();
+        set.add(20);
+        set.add(10);
+        System.out.println(set);
+    }
+}
