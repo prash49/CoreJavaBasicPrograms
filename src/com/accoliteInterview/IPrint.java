4c691758adf94e6f7e364d9425a0abc7270087d5
Prashanth N <imprash4u@gmail.com>
2025-01-01 20:31:44 +0530

    accolite interview

---
+++ src/com/accoliteInterview/IPrint.java
@@ -0,0 +1,15 @@
+//package com.accoliteInterview;
+//
+//public interface IPrint{
+//protected void printMe();
+//}
+//
+//public class PrintTest implements IPrint{
+//public String printMe(){
+//System.out.println("Me");
+//}
+//public static void main (String[] args){
+//PrintTest test = new PrintTest();
+//test.printMe();
+//}
+//}
